37949c1462a4c3e4c79a769a9c6ca30bb8f1d7ee
Parabeac-Cloud <admin@parabeac.com>
2023-04-30 18:26:42 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/chat_app_colors.g.dart
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ChatAppColors {
+
+static const Color primary = Color(0xff771f98);
+
+static const Color labelColorLightPrimary = Color(0xff000000);
+
+static const Color tertiary = Color(0xff993f3f);
+
+static const Color secondary = Color(0xff771f98);
+
+static const Color background = Color(0xffffffff);
+}
---
+++ lib/theme/chat_app_text_styles.g.dart
@@ -0,0 +1,100 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ChatAppTextStyles {
+
+static const TextStyle headline1 = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodySmall = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyMedium = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyBold = TextStyle(
+  fontSize: 17,
+  fontWeight: FontWeight.w600,
+  letterSpacing: -0.40799999237060547,
+  fontFamily: 'SF Pro Text',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyLarge = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyText2 = TextStyle(
+  fontSize: 13,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyText1 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline2 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline3 = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
