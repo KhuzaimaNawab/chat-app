810aea5a7c5684227becd73153e1af3f5b668c0b
Parabeac-Cloud <admin@parabeac.com>
2023-04-30 17:45:57 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/chat_app_colors.g.dart
@@ -0,0 +1,12 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ChatAppColors {
+
+static const Color labelColorLightPrimary = Color(0xff000000);
+}
